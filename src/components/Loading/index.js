f66f3f86914735cbe794cebc2b9191e674ce5df0
charles <charles8xu@gmail.com>
2018-04-20 12:28:36 +0800

    adjust game map

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import styled, { keyframes } from 'styled-components'
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
 
-import { Centered } from '../Layout'
-import { COLOR_RED, COLOR_BLUE } from '../../utils/constants'
+import { Centered } from '../Layout';
+import { COLOR_RED, COLOR_BLUE } from '../../utils/constants';
 
 const flickerOpacity = keyframes`
   0% {
@@ -21,7 +21,7 @@ const flickerOpacity = keyframes`
     opacity: 0;
     transform: translate(0, 0);
   }
-`
+`;
 
 const flicker = keyframes`
   0% {
@@ -33,7 +33,7 @@ const flicker = keyframes`
   100% {
     transform: translate(0, 0);
   }
-`
+`;
 
 const Dot = styled.div`
       position: absolute;
@@ -47,7 +47,7 @@ const Dot = styled.div`
         props.animate &&
         'animation: ' + props.animate + ' 1.9s linear infinite;'}
       ${props => props.delay && 'animation-delay: ' + props.delay + ';'}
-`
+`;
 
 const Logo = () => (
   <div
@@ -61,17 +61,17 @@ const Logo = () => (
     <Dot color={COLOR_BLUE} animate={flicker} delay="0s" />
     <Dot color={COLOR_RED} animate={flickerOpacity} delay="-0.95s" />
   </div>
-)
+);
 
 const Text = styled.span`
   color: #60626e;
-  font-size: 1.5rem;
+  font-size: 24px;
   text-align: center;
-`
+`;
 
 export default () => (
   <Centered vertical horizontal color="#ECE0CC">
     <Logo />
     <Text>正在收集点点...</Text>
   </Centered>
-)
+);
