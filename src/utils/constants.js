53cc2c89b6939739d4af7de6e3cb450d554c8548
charles <charles8xu@gmail.com>
2018-06-27 17:46:10 +0800

    adjust sRGB color

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,17 +1,17 @@
 // dot color
-export const COLOR_RED = '#E75F62'
-export const COLOR_YELLOW = '#F2BF66'
-export const COLOR_BLUE = '#6F89B9'
-export const COLOR_PURPLE = '#A45A7D'
-export const COLOR_GREEN = '#7EB69D'
-export const COLOR_TRANS = '#00FFFFFF'
+export const COLOR_RED = '#E84D60';
+export const COLOR_YELLOW = '#FECD6C';
+export const COLOR_BLUE = '#718DBF';
+export const COLOR_PURPLE = '#A4547D';
+export const COLOR_GREEN = '#77C298';
+export const COLOR_TRANS = '#00FFFFFF';
 
-export const DIRECTION_LEFT = 'DIRECTION_LEFT'
-export const DIRECTION_RIGHT = 'DIRECTION_RIGHT'
-export const DIRECTION_UP = 'DIRECTION_UP'
-export const DIRECTION_DOWN = 'DIRECTION_DOWN'
-export const DIRECTION_NONE = 'DIRECTION_NONE'
+export const DIRECTION_LEFT = 'DIRECTION_LEFT';
+export const DIRECTION_RIGHT = 'DIRECTION_RIGHT';
+export const DIRECTION_UP = 'DIRECTION_UP';
+export const DIRECTION_DOWN = 'DIRECTION_DOWN';
+export const DIRECTION_NONE = 'DIRECTION_NONE';
 
 // dot type
-export const DOT_TYPE_EMPTY = 0
-export const DOT_TYPE_DOT = 1
+export const DOT_TYPE_EMPTY = 0;
+export const DOT_TYPE_DOT = 1;
